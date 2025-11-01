91d55ab94d379e27dd673735bf0916fae6aec2d3
Mustafa Siraj Shaikh <69889801+smsirajk@users.noreply.github.com>
2025-11-01 07:30:54 +0530

    Add files via upload

---
+++ hackerrank - Python/BuiltIns/PythonEvaluation.py
@@ -0,0 +1,9 @@
+"""
+Title     : Python Evaluation
+Subdomain : Built-Ins
+Domain    : Python
+Author    : smsirajk
+Created   : 15 July 2016
+Problem   : https://www.hackerrank.com/challenges/python-eval/problem
+"""
+eval(input())
